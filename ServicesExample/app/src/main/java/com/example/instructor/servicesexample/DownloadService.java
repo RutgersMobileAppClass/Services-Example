f3deda452e83e5eea84ae9099c772d0af96edc7b
GDC <gradeighdclark@gmail.com>
2016-10-14 13:00:09 -0400

    initial

---
+++ ServicesExample/app/src/main/java/com/example/instructor/servicesexample/DownloadService.java
@@ -0,0 +1,86 @@
+package com.example.instructor.servicesexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DownloadService extends Service {
+    public DownloadService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        final String link = intent.getStringExtra("link");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                downloadFile(link);
+
+            }
+        });
+
+
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+
+    public void downloadFile(String link){
+        try {
+            URL url = new URL(link);
+
+            // extract the end of the URL file name
+            // Example : www.google.com/keylogger.exe, filename = keylogger.exe
+            String filename = new File(url.getFile()).getName();
+            File file =  new File(Environment.getExternalStorageDirectory(),filename);
+
+            OutputStream output = null;
+
+            try {
+                output = new PrintStream(file);
+            } catch (FileNotFoundException e) {
+                Log.wtf("File writing", e);
+                e.printStackTrace();
+            }
+
+            try {
+                InputStream input = new BufferedInputStream(url.openStream());
+                byte[] buffer = new byte[512000];
+                int numRead;
+                while((numRead = input.read(buffer)) != -1){
+                    output.write(buffer,0,numRead);
+                }
+                output.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (MalformedURLException e) {
+            Log.wtf("URL Download",e);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+
+        return null;
+
+    }
+
+
+}
---
+++ ServicesExample/app/src/main/java/com/example/instructor/servicesexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.instructor.servicesexample;
+
+import android.content.Intent;
+import android.icu.util.Output;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText linkText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linkText = (EditText) findViewById(R.id.linkText);
+
+    }
+
+    public void downloadClick(View view) {
+        final String link = linkText.getText().toString();
+        Intent intent = new Intent(this,DownloadService.class);
+        intent.putExtra("link",link);
+
+        startService(intent);
+
+    }
+
+
+
+}
